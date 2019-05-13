340d0f42cf59ca7a47e24400e11c91f4874aeb6c
Dirk Kiesewetter <dirk.k1@outlook.com>
2019-05-13 16:18:59 -0400

    updated code to use ES6 modules

--- assets/javascript/app_refactored.js
+++ assets/javascript/app_refactored.js
@@ -1,85 +1,91 @@
-console.log("app.js active");
+import questions from './questions.js'
+
+console.log('app.js active')
 
 $(document).ready(() => {
-  let correctAnswers = 0;
-  let wrongAnswers = 0;
-  let unanswered = 0;
-  let timer = 51;
+  let correctAnswers = 0
+  let wrongAnswers = 0
+  let unanswered = 0
+  let timer = 51
 
   const gameStart = () => {
-    $("#gameBoard").hide();
-    $("#scoreScreen").hide();
-    console.log("gameStart running");
-  };
+    $('#gameBoard').hide()
+    $('#scoreScreen').hide()
+    console.log('gameStart running')
+  }
 
   const run = () => {
-    intervalId = setInterval(decrement, 1000);
-  };
+    intervalId = setInterval(decrement, 1000)
+  }
 
   const decrement = () => {
-    timer--;
-    $("#countdown").html(`Time remaining: ${timer} seconds`);
+    timer--
+    $('#countdown').html(`Time remaining: ${timer} seconds`)
     if (timer === 0) {
-      scoring();
-      $("#gameBoard").remove();
-      $("#scoreScreen").show();
+      scoring()
+      $('#gameBoard').remove()
+      $('#scoreScreen').show()
     }
-  };
+  }
 
   const scoring = () => {
     for (let i = 0; i < questions.length; i++) {
-      //added :checked to indicate the checked radio box
-      let questionAnswer = $(`input[name=question${[i]}]:checked`).val();
+      // added :checked to indicate the checked radio box
+      let questionAnswer = $(`input[name=question${[i]}]:checked`).val()
 
       if (questionAnswer === questions[i].correctAnswer) {
-        correctAnswers++;
+        correctAnswers++
       } else if (
         questionAnswer !== questions[i].correctAnswer &&
         questionAnswer !== undefined
       ) {
-        wrongAnswers++;
+        wrongAnswers++
       } else {
-        unanswered++;
+        unanswered++
       }
-      console.log("scoring function called");
+      console.log('scoring function called')
     }
 
-    $("#correctAnswers").append(correctAnswers);
-    $("#wrongAnswers").append(wrongAnswers);
-    $("#unanswered").append(unanswered);
-  };
+    $('#correctAnswers').append(correctAnswers)
+    $('#wrongAnswers').append(wrongAnswers)
+    $('#unanswered').append(unanswered)
+  }
 
   // removes start button onclick and displays question screen
-  gameStart();
+  gameStart()
 
-  $("#startButton").click(() => {
-    $("#startScreen").hide();
-    $("#gameBoard").show();
-    run();
-    decrement();
-    console.log("start button click running");
+  $('#startButton').click(() => {
+    $('#startScreen').hide()
+    $('#gameBoard').show()
+    run()
+    decrement()
+    console.log('start button click running')
 
-    //outer for loop iterates through each question
+    // outer for loop iterates through each question
     for (let j = 0; j < questions.length; j++) {
-      $("#questionDiv").append(
-        `<p class='questionCopy'>${questions[j].question}</p>`);
+      $('#questionDiv').append(
+        `<p class='questionCopy'>${questions[j].question}</p>`
+      )
 
-      //inner for loop iterates through the answer array of each question
+      // inner for loop iterates through the answer array of each question
       for (let k = 0; k < questions[j].answers.length; k++) {
-        $("#questionDiv").append(
-          `<input type="radio" class="radio-btn" name="question${j}" value="${questions[j].answers[k]}">${questions[j].answers[k]}`);
+        $('#questionDiv').append(
+          `<input type="radio" class="radio-btn" name="question${j}" value="${
+            questions[j].answers[k]
+          }">${questions[j].answers[k]}`
+        )
       }
-      $("#questionDiv").append("<br>");
-      $("#questionDiv").append("<hr>");
+      $('#questionDiv').append('<br>')
+      $('#questionDiv').append('<hr>')
     }
-  });
+  })
 
   // clears question screen takes user to scoring screen
   // timer = 0 added to prevent scoring function from running twice
-  $("#doneButton").click(() => {
-    timer = 0;
-    scoring();
-    $("#gameBoard").remove();
-    $("#scoreScreen").show();
-  });
-});
+  $('#doneButton').click(() => {
+    timer = 0
+    scoring()
+    $('#gameBoard').remove()
+    $('#scoreScreen').show()
+  })
+})
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,42 +1,44 @@
 const questions = [
   {
-    question: "What constellation is also known as the Seven Sisters?",
-    answers: ["Big Dipper", "Pleiades", "Cassiopeia"],
-    correctAnswer: "Pleiades"
+    question: 'What constellation is also known as the Seven Sisters?',
+    answers: ['Big Dipper', 'Pleiades', 'Cassiopeia'],
+    correctAnswer: 'Pleiades'
   },
   {
-    question: "What is the distance from the Earth to the Sun?",
-    answers: ["93 million miles", "1 light year", "500,000 miles"],
-    correctAnswer: "93 million miles"
+    question: 'What is the distance from the Earth to the Sun?',
+    answers: ['93 million miles', '1 light year', '500,000 miles'],
+    correctAnswer: '93 million miles'
   },
   {
-    question: "What is the largest known star?",
-    answers: ["Sirius", "Betelgeuse", "VY Canis Majoris"],
-    correctAnswer: "VY Canis Majoris"
+    question: 'What is the largest known star?',
+    answers: ['Sirius', 'Betelgeuse', 'VY Canis Majoris'],
+    correctAnswer: 'VY Canis Majoris'
   },
   {
-    question: "What is the closest star to Earth?",
-    answers: ["Alpha Centauri", "Proxima Centauri", "Barnard's Star"],
-    correctAnswer: "Proxima Centauri"
+    question: 'What is the closest star to Earth?',
+    answers: ['Alpha Centauri', 'Proxima Centauri', "Barnard's Star"],
+    correctAnswer: 'Proxima Centauri'
   },
   {
-    question: "How much larger is the Sun than Earth?",
-    answers: ["750 times", "5,000 times", "300,000 times"],
-    correctAnswer: "300,000 times"
+    question: 'How much larger is the Sun than Earth?',
+    answers: ['750 times', '5,000 times', '300,000 times'],
+    correctAnswer: '300,000 times'
   },
   {
-    question: "How many moons does Jupiter have?",
-    answers: ["79", "8", "26"],
-    correctAnswer: "79"
+    question: 'How many moons does Jupiter have?',
+    answers: ['79', '8', '26'],
+    correctAnswer: '79'
   },
   {
-    question: "What is a shooting star?",
-    answers: ["comet", "meteor", "asteroid"],
-    correctAnswer: "meteor"
+    question: 'What is a shooting star?',
+    answers: ['comet', 'meteor', 'asteroid'],
+    correctAnswer: 'meteor'
   },
   {
-    question: "How many planets in our solar system have rings?",
-    answers: ["1", "2", "4"],
-    correctAnswer: "4"
+    question: 'How many planets in our solar system have rings?',
+    answers: ['1', '2', '4'],
+    correctAnswer: '4'
   }
-];
+]
+
+export { questions }
